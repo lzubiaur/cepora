f028b66b64462e94eb26a3d0ee3270ca848b8bbf
lzubiaur <zubiaur.laurent@gmail.com>
2015-12-22 10:57:48 +0100

    Add build osx script

---
+++ bin/build-osx.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if ! [ -d "build/osx" ]
+then
+   mkdir -p "build/osx"
+fi
+cd build/osx
+
+# Generate build system using the XCode generator.
+# Should also work using the "Unix Makefiles" generator.
+#cmake -G "Xcode" \
+cmake -G "Unix Makefiles" \
+    -DBUILD_OSX=TRUE \
+    -DCMAKE_BUILD_TYPE=Release \
+    ../..
+
+# Build and install the project using the Release config
+cmake --build . --target install --config Release
