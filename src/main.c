7881504b34e3cf8c5b22f69cead85c3134ba1c33
lzubiaur <zubiaur.laurent@gmail.com>
2015-12-24 10:41:03 +0100

    Pass cmd line arguments to Javascript

--- src/main.c
+++ src/main.c
@@ -3,14 +3,28 @@
 
 #include "duktape.h"
 
-int readfile(duk_context *ctx)
+duk_ret_t readfile(duk_context *ctx)
 {
   FILE *fp = NULL;
   char *str = NULL;
   long fsize = 0;
-  int n = duk_get_top(ctx);  /* #args passed to the function */
 
-  // TODO check parameters
+  /* It's not not mandatory to check the number of parameters passed to this
+  * function because it's guaranteed to be one (the stack will have one
+  * and only one value (see how the function is binded using
+  * `duk_push_c_function`)/
+  /*
+  if (duk_get_top(ctx) == 0) {
+    return DUK_RET_TYPE_ERROR;
+  }
+  */
+
+  /* Throw `TypeError` if no arguments is given.
+  * Check if the parameter is either `null` or `undefined`. There is only one
+  * parameter so we can check the top of the stack (-1) */
+  if (duk_is_null_or_undefined(ctx, -1)) {
+    return DUK_RET_TYPE_ERROR;
+  }
 
   const char *filename = duk_to_string(ctx, 0);
 
@@ -30,7 +44,7 @@ int readfile(duk_context *ctx)
 
   fclose(fp);
   duk_push_lstring(ctx, str, fsize);
-  return 1;  /* one return value */
+  return 1;  /* return one value */
 }
 
 int main(int argc, char *argv[]) {
@@ -42,10 +56,36 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
+  /* Note regarding function binding parameters.
+  * The third argument of `duk_push_c_function` is the number of parameters the
+  * C function expects. It can be variable (DUK_VARARGS) or set to a
+  * maximum number of parameters.
+  * If a maximum of parameters is explicitly defined then the C functions will
+  * get a value stack with all the expected parameters with the missing
+  * parameters set to `undefined` and the extra parameter ignored.
+  * If DUK_VARARGS is used then `duk_get_top` will return the number of
+  * parameter passed.
+  */
   duk_push_global_object(ctx);
-  duk_push_c_function(ctx, readfile, 1); /* One argument */
-  duk_put_prop_string(ctx, -2 /*idx:global*/, "readfile");
+  duk_push_c_function(ctx, readfile, 1); /* C function with exactly one argument */
+  duk_put_prop_string(ctx, -2 /*index of global*/, "readfile");
   duk_pop(ctx);  /* pop global */
+
+  /* Store command line arguments in the `Duktape` global object */
+  duk_push_global_object(ctx);
+  duk_get_prop_string(ctx, -1, "Duktape");
+  duk_push_string(ctx, "argv");
+  duk_idx_t arr_idx = duk_push_array(ctx); /* push an empty array */
+
+  for (int i=0; i<argc; ++i) {
+    duk_push_string(ctx, argv[i]);
+    duk_put_prop_index(ctx, arr_idx, i);
+  }
+
+  duk_def_prop(ctx, -3, DUK_DEFPROP_HAVE_VALUE); /* Non writable property */
+  duk_pop_2(ctx);
+
+  /* Run the script entry point */
 
   if (duk_peval_file(ctx, "js/process.js") != 0) {
     printf("Error: %s\n", duk_safe_to_string(ctx, -1));
