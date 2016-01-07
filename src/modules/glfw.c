29968ababf84a4b0c7a66f3b2cda6aa00edae51d
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-07 20:18:39 +0100

    Add glfw module files

---
+++ src/modules/glfw.c
@@ -0,0 +1,7 @@
+/*
+ * cpr_glfw.c
+ * Copyright (c) 2015 Laurent Zubiaur
+ * MIT License (http://opensource.org/licenses/MIT)
+ */
+
+#include "duktape.h"
---
+++ src/modules/glfw.h
@@ -0,0 +1,14 @@
+/*
+ * cpr_glfw.h
+ * Copyright (c) 2015 Laurent Zubiaur
+ * MIT License (http://opensource.org/licenses/MIT)
+ */
+
+#ifndef CPR_GLFW_H
+#define CPR_GLFW_H
+
+#include "duktape.h"
+
+duk_ret_t dukopen_glfw(duk_context *ctx);
+
+#endif /* CPR_GLFW_H */
