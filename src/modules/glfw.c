2214f51373ab6516cb6127e16a5ad59226d13d00
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-07 20:25:22 +0100

    Bind glfwInit

--- src/modules/glfw.c
+++ src/modules/glfw.c
@@ -5,3 +5,20 @@
  */
 
 #include "duktape.h"
+#include "glfw.h"
+
+duk_ret_t glfw_init(duk_context *ctx) {
+  glfwInit();
+}
+
+static const duk_function_list_entry module_funcs[] = {
+    { "init", glfw_init, 0 },
+    { NULL, NULL, 0 }
+};
+
+duk_ret_t dukopen_io(duk_context *ctx) {
+  duk_push_object(ctx);  /* module result */
+  duk_put_function_list(ctx, -1, module_funcs);
+
+  return 1;  /* return module value */
+}
