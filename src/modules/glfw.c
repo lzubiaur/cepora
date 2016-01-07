cc9a2b6ca826a80c4c3ccf590c97675ba2487d83
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-07 21:20:33 +0100

    Fix open function name typo

--- src/modules/glfw.c
+++ src/modules/glfw.c
@@ -8,14 +8,14 @@
 #include "glfw.h"
 #include "GLFW/glfw3.h"
 
-duk_ret_t glfw_init(duk_context *ctx) {
+static duk_ret_t glfw_init(duk_context *ctx) {
   int rc;
   rc = glfwInit();
   duk_push_boolean(ctx, rc);
   return 1;
 }
 
-duk_ret_t glfw_terminate(duk_context *ctx) {
+static duk_ret_t glfw_terminate(duk_context *ctx) {
   glfwTerminate();
   return 0;
 }
@@ -26,7 +26,7 @@ static const duk_function_list_entry module_funcs[] = {
     { NULL, NULL, 0 }
 };
 
-duk_ret_t dukopen_io(duk_context *ctx) {
+duk_ret_t dukopen_glfw(duk_context *ctx) {
   duk_push_object(ctx);  /* module result */
   duk_put_function_list(ctx, -1, module_funcs);
 
