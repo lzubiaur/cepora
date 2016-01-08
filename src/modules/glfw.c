31b39445ba9d60739238d7bb313f59aa86421c67
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-08 21:07:47 +0100

    Add bindings

--- src/modules/glfw.c
+++ src/modules/glfw.c
@@ -8,6 +8,15 @@
 #include "glfw.h"
 #include "GLFW/glfw3.h"
 
+#include "cpr_macros.h"
+
+#define GLFW_ERR_CALLBACK_STASH_KEY "glfwErrCallbackKey"
+#define GLFW_KEY_CALLBACK_STASH_KEY "glfwKeyCallbackKey"
+
+/* global reference to the duktape context. Required for GLFW callbacks */
+/* XXX is there any workaround to avoid keeping a global reference to the context? */
+static duk_context *_ctx = NULL;
+
 static duk_ret_t glfw_init(duk_context *ctx) {
   int rc;
   rc = glfwInit();
@@ -43,47 +52,108 @@ static duk_ret_t glfw_make_context_current(duk_context *ctx)
   return 0;
 }
 
-static duk_context *error_ctx = NULL;
-static void *error_callback_ptr = NULL;
-
 void error_callback(int error, const char* description)
 {
-  duk_push_heapptr(error_ctx, error_callback_ptr);
-  duk_push_int(error_ctx, error);
-  duk_push_string(error_ctx, description);
-  duk_call(error_ctx, 2);
+  duk_push_global_stash(_ctx);
+  duk_get_prop_string(_ctx, -1, GLFW_ERR_CALLBACK_STASH_KEY);
+  duk_push_int(_ctx, error);
+  duk_push_string(_ctx, description);
+  duk_call(_ctx, 2);
 }
 
 static duk_ret_t glfw_set_error_callback(duk_context *ctx) {
-  // duk_is_ecmascript_function(ctx, 0);
-  error_ctx = ctx;
-  error_callback_ptr = duk_get_heapptr(ctx, 0);
+  if (duk_is_function(ctx, 0) == 0) {
+    duk_error(ctx, DUK_ERR_TYPE_ERROR, "not a function");
+  }
+  duk_push_global_stash(ctx);
+  duk_dup(ctx, 0); /* Push the error function callback  */
+  duk_put_prop_string(ctx, -2, GLFW_ERR_CALLBACK_STASH_KEY);
+
   glfwSetErrorCallback(error_callback);
   return 0;
 }
 
 static duk_ret_t glfw_window_should_close(duk_context *ctx) {
   int rc = 0;
-  void * window = NULL;
-  window = duk_require_pointer(ctx, 0);
-  rc = glfwWindowShouldClose(window);
+  rc = glfwWindowShouldClose(duk_require_pointer(ctx ,0));
   duk_push_boolean(ctx, rc);
+  return 1;
+}
+
+static duk_ret_t glfw_destroy_window(duk_context *ctx) {
+  glfwDestroyWindow(duk_require_pointer(ctx, 0));
+  return 0;
+}
+
+static duk_ret_t glfw_swap_buffers(duk_context *ctx) {
+  glfwSwapBuffers(duk_require_pointer(ctx, 0));
+  return 0;
+}
+
+static duk_ret_t glfw_pool_events(duk_context *ctx) {
+  glfwPollEvents();
+  return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  duk_push_global_stash(_ctx);
+  duk_get_prop_string(_ctx, -1, GLFW_KEY_CALLBACK_STASH_KEY);
+  duk_push_pointer(_ctx, window);
+  duk_push_int(_ctx, key);
+  duk_push_int(_ctx, scancode);
+  duk_push_int(_ctx, action);
+  duk_push_int(_ctx, mods);
+  duk_call(_ctx, 5);
+}
+
+static duk_ret_t glfw_set_key_callback(duk_context *ctx) {
+  if (duk_is_function(ctx, 1) == 0) {
+    duk_error(ctx, DUK_ERR_TYPE_ERROR, "not a function");
+  }
+  duk_push_global_stash(ctx);
+  duk_dup(ctx, 1); /* Push the callback function */
+  duk_put_prop_string(ctx, -2, GLFW_KEY_CALLBACK_STASH_KEY);
+
+  glfwSetKeyCallback(duk_require_pointer(ctx, 0), key_callback);
+  return 0;
+}
+
+static duk_ret_t glfw_set_window_should_close(duk_context *ctx) {
+  glfwSetWindowShouldClose(duk_require_pointer(ctx, 0), duk_require_boolean(ctx, 1));
   return 0;
 }
 
 static const duk_function_list_entry module_funcs[] = {
-    { "init",               glfw_init,                  0 },
-    { "terminate",          glfw_terminate,             0 },
-    { "createWindow",       glfw_create_window,         3 },
-    { "makeContextCurrent", glfw_make_context_current,  1 },
-    { "setErrorCallBack",   glfw_set_error_callback,    1 },
-    { "windowShouldClose",  glfw_window_should_close,   1 },
+    { "init",                 glfw_init,                    0 },
+    { "terminate",            glfw_terminate,               0 },
+    { "createWindow",         glfw_create_window,           3 },
+    { "makeContextCurrent",   glfw_make_context_current,    1 },
+    { "setErrorCallBack",     glfw_set_error_callback,      1 },
+    { "windowShouldClose",    glfw_window_should_close,     1 },
+    { "destroyWindow",        glfw_destroy_window,          1 },
+    { "swapBuffers",          glfw_swap_buffers,            1 },
+    { "poolEvents",           glfw_pool_events,             0 },
+    { "setKeyCallback",       glfw_set_key_callback,        2 },
+    { "setWindowShouldClose", glfw_set_window_should_close, 2 },
     { NULL, NULL, 0 }
+};
+
+const duk_number_list_entry module_consts[] = {
+    { "GLFW_RELEASE",       0   },
+    { "GLFW_PRESS",         1   },
+    { "GLFW_REPEAT",        2   },
+    { "GLFW_KEY_UNKNOWN",   -1  },
+    { "GLFW_KEY_ESCAPE",    256 },
+    { NULL, 0.0 }
 };
 
 duk_ret_t dukopen_glfw(duk_context *ctx) {
   duk_push_object(ctx);  /* module result */
   duk_put_function_list(ctx, -1, module_funcs);
+  duk_put_number_list(ctx, -1, module_consts);
+
+  _ctx = ctx;
 
   return 1;  /* return module value */
 }
