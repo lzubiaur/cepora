ba99edb067a6760c0f2066ff2a9ee9d2e45f901c
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-09 19:45:10 +0100

    Bind context handling functions

--- src/modules/glfw.c
+++ src/modules/glfw.c
@@ -18,6 +18,37 @@
 /* TODO use duk_get_heapptr + duk_push_heapptr for performance */
 static duk_context *_ctx = NULL;
 
+/* Context handling */
+
+static duk_ret_t glfw_make_context_current(duk_context *ctx) {
+  GLFWwindow *window = NULL;
+  window = duk_require_pointer(ctx, 0);
+  glfwMakeContextCurrent(window);
+  return 0;
+}
+
+static duk_ret_t glfw_get_current_context(duk_context *ctx) {
+  duk_push_pointer(ctx, glfwGetCurrentContext());
+  return 1;
+}
+
+static duk_ret_t glfw_swap_interval(duk_context *ctx) {
+  glfwSwapInterval(duk_require_int(ctx, -1));
+  return 0;
+}
+
+#if defined(CPR__GLFW_MANUAL_EXT_LOADING)
+static duk_ret_t glfw_extension_supported(duk_context *ctx) {
+  duk_push_boolean(ctx, glfwExtensionSupported(duk_get_string(ctx, 0)));
+  return 1;
+}
+
+static duk_ret_t glfw_get_proc_address(duk_context *ctx) {
+  duk_push_pointer(ctx, glfwGetProcAddress(duk_get_string(ctx, 0)));
+  return 1;
+}
+#endif
+
 static duk_ret_t glfw_init(duk_context *ctx) {
   int rc;
   rc = glfwInit();
@@ -43,14 +74,6 @@ static duk_ret_t glfw_create_window(duk_context *ctx) {
   window = glfwCreateWindow(width, height, title, NULL, NULL);
   duk_push_pointer(ctx, window);
   return 1;
-}
-
-static duk_ret_t glfw_make_context_current(duk_context *ctx)
-{
-  GLFWwindow *window = NULL;
-  window = duk_require_pointer(ctx, 0);
-  glfwMakeContextCurrent(window);
-  return 0;
 }
 
 void error_callback(int error, const char* description)
@@ -126,10 +149,17 @@ static duk_ret_t glfw_set_window_should_close(duk_context *ctx) {
 }
 
 static const duk_function_list_entry module_funcs[] = {
+  /* Context handling */
+  { "makeContextCurrent",   glfw_make_context_current,    1 },
+  { "getCurrentContext",    glfw_get_current_context,     0 },
+  { "swapInterval",         glfw_swap_interval,           1 },
+#if defined(CPR__GLFW_MANUAL_EXT_LOADING)
+  { "extensionSupported",   glfw_extension_supported,     1 },
+  { "getProcAddress",       glfw_get_proc_address,        1 },
+#endif
   { "init",                 glfw_init,                    0 },
   { "terminate",            glfw_terminate,               0 },
   { "createWindow",         glfw_create_window,           3 },
-  { "makeContextCurrent",   glfw_make_context_current,    1 },
   { "setErrorCallBack",     glfw_set_error_callback,      1 },
   { "windowShouldClose",    glfw_window_should_close,     1 },
   { "destroyWindow",        glfw_destroy_window,          1 },
@@ -141,11 +171,234 @@ static const duk_function_list_entry module_funcs[] = {
 };
 
 const duk_number_list_entry module_consts[] = {
-  { "GLFW_RELEASE",       (double)0   },
-  { "GLFW_PRESS",         (double)1   },
-  { "GLFW_REPEAT",        (double)2   },
-  { "GLFW_KEY_UNKNOWN",   (double)-1  },
-  { "GLFW_KEY_ESCAPE",    (double)256 },
+  { "VERSION_MAJOR",               (double) GLFW_VERSION_MAJOR },
+  { "VERSION_MINOR",               (double) GLFW_VERSION_MINOR },
+  { "VERSION_REVISION",            (double) GLFW_VERSION_REVISION },
+  { "RELEASE",                     (double) GLFW_RELEASE },
+  { "PRESS",                       (double) GLFW_PRESS },
+  { "REPEAT",                      (double) GLFW_REPEAT },
+  { "KEY_UNKNOWN",                 (double) GLFW_KEY_UNKNOWN },
+  { "KEY_SPACE",                   (double) GLFW_KEY_SPACE },
+  { "KEY_APOSTROPHE",              (double) GLFW_KEY_APOSTROPHE },
+  { "KEY_COMMA",                   (double) GLFW_KEY_COMMA },
+  { "KEY_MINUS",                   (double) GLFW_KEY_MINUS },
+  { "KEY_PERIOD",                  (double) GLFW_KEY_PERIOD },
+  { "KEY_SLASH",                   (double) GLFW_KEY_SLASH },
+  { "KEY_0",                       (double) GLFW_KEY_0 },
+  { "KEY_1",                       (double) GLFW_KEY_1 },
+  { "KEY_2",                       (double) GLFW_KEY_2 },
+  { "KEY_3",                       (double) GLFW_KEY_3 },
+  { "KEY_4",                       (double) GLFW_KEY_4 },
+  { "KEY_5",                       (double) GLFW_KEY_5 },
+  { "KEY_6",                       (double) GLFW_KEY_6 },
+  { "KEY_7",                       (double) GLFW_KEY_7 },
+  { "KEY_8",                       (double) GLFW_KEY_8 },
+  { "KEY_9",                       (double) GLFW_KEY_9 },
+  { "KEY_SEMICOLON",               (double) GLFW_KEY_SEMICOLON },
+  { "KEY_EQUAL",                   (double) GLFW_KEY_EQUAL },
+  { "KEY_A",                       (double) GLFW_KEY_A },
+  { "KEY_B",                       (double) GLFW_KEY_B },
+  { "KEY_C",                       (double) GLFW_KEY_C },
+  { "KEY_D",                       (double) GLFW_KEY_D },
+  { "KEY_E",                       (double) GLFW_KEY_E },
+  { "KEY_F",                       (double) GLFW_KEY_F },
+  { "KEY_G",                       (double) GLFW_KEY_G },
+  { "KEY_H",                       (double) GLFW_KEY_H },
+  { "KEY_I",                       (double) GLFW_KEY_I },
+  { "KEY_J",                       (double) GLFW_KEY_J },
+  { "KEY_K",                       (double) GLFW_KEY_K },
+  { "KEY_L",                       (double) GLFW_KEY_L },
+  { "KEY_M",                       (double) GLFW_KEY_M },
+  { "KEY_N",                       (double) GLFW_KEY_N },
+  { "KEY_O",                       (double) GLFW_KEY_O },
+  { "KEY_P",                       (double) GLFW_KEY_P },
+  { "KEY_Q",                       (double) GLFW_KEY_Q },
+  { "KEY_R",                       (double) GLFW_KEY_R },
+  { "KEY_S",                       (double) GLFW_KEY_S },
+  { "KEY_T",                       (double) GLFW_KEY_T },
+  { "KEY_U",                       (double) GLFW_KEY_U },
+  { "KEY_V",                       (double) GLFW_KEY_V },
+  { "KEY_W",                       (double) GLFW_KEY_W },
+  { "KEY_X",                       (double) GLFW_KEY_X },
+  { "KEY_Y",                       (double) GLFW_KEY_Y },
+  { "KEY_Z",                       (double) GLFW_KEY_Z },
+  { "KEY_LEFT_BRACKET",            (double) GLFW_KEY_LEFT_BRACKET },
+  { "KEY_BACKSLASH",               (double) GLFW_KEY_BACKSLASH },
+  { "KEY_RIGHT_BRACKET",           (double) GLFW_KEY_RIGHT_BRACKET },
+  { "KEY_GRAVE_ACCENT",            (double) GLFW_KEY_GRAVE_ACCENT },
+  { "KEY_WORLD_1",                 (double) GLFW_KEY_WORLD_1 },
+  { "KEY_WORLD_2",                 (double) GLFW_KEY_WORLD_2 },
+  { "KEY_ESCAPE",                  (double) GLFW_KEY_ESCAPE },
+  { "KEY_ENTER",                   (double) GLFW_KEY_ENTER },
+  { "KEY_TAB",                     (double) GLFW_KEY_TAB },
+  { "KEY_BACKSPACE",               (double) GLFW_KEY_BACKSPACE },
+  { "KEY_INSERT",                  (double) GLFW_KEY_INSERT },
+  { "KEY_DELETE",                  (double) GLFW_KEY_DELETE },
+  { "KEY_RIGHT",                   (double) GLFW_KEY_RIGHT },
+  { "KEY_LEFT",                    (double) GLFW_KEY_LEFT },
+  { "KEY_DOWN",                    (double) GLFW_KEY_DOWN },
+  { "KEY_UP",                      (double) GLFW_KEY_UP },
+  { "KEY_PAGE_UP",                 (double) GLFW_KEY_PAGE_UP },
+  { "KEY_PAGE_DOWN",               (double) GLFW_KEY_PAGE_DOWN },
+  { "KEY_HOME",                    (double) GLFW_KEY_HOME },
+  { "KEY_END",                     (double) GLFW_KEY_END },
+  { "KEY_CAPS_LOCK",               (double) GLFW_KEY_CAPS_LOCK },
+  { "KEY_SCROLL_LOCK",             (double) GLFW_KEY_SCROLL_LOCK },
+  { "KEY_NUM_LOCK",                (double) GLFW_KEY_NUM_LOCK },
+  { "KEY_PRINT_SCREEN",            (double) GLFW_KEY_PRINT_SCREEN },
+  { "KEY_PAUSE",                   (double) GLFW_KEY_PAUSE },
+  { "KEY_F1",                      (double) GLFW_KEY_F1 },
+  { "KEY_F2",                      (double) GLFW_KEY_F2 },
+  { "KEY_F3",                      (double) GLFW_KEY_F3 },
+  { "KEY_F4",                      (double) GLFW_KEY_F4 },
+  { "KEY_F5",                      (double) GLFW_KEY_F5 },
+  { "KEY_F6",                      (double) GLFW_KEY_F6 },
+  { "KEY_F7",                      (double) GLFW_KEY_F7 },
+  { "KEY_F8",                      (double) GLFW_KEY_F8 },
+  { "KEY_F9",                      (double) GLFW_KEY_F9 },
+  { "KEY_F10",                     (double) GLFW_KEY_F10 },
+  { "KEY_F11",                     (double) GLFW_KEY_F11 },
+  { "KEY_F12",                     (double) GLFW_KEY_F12 },
+  { "KEY_F13",                     (double) GLFW_KEY_F13 },
+  { "KEY_F14",                     (double) GLFW_KEY_F14 },
+  { "KEY_F15",                     (double) GLFW_KEY_F15 },
+  { "KEY_F16",                     (double) GLFW_KEY_F16 },
+  { "KEY_F17",                     (double) GLFW_KEY_F17 },
+  { "KEY_F18",                     (double) GLFW_KEY_F18 },
+  { "KEY_F19",                     (double) GLFW_KEY_F19 },
+  { "KEY_F20",                     (double) GLFW_KEY_F20 },
+  { "KEY_F21",                     (double) GLFW_KEY_F21 },
+  { "KEY_F22",                     (double) GLFW_KEY_F22 },
+  { "KEY_F23",                     (double) GLFW_KEY_F23 },
+  { "KEY_F24",                     (double) GLFW_KEY_F24 },
+  { "KEY_F25",                     (double) GLFW_KEY_F25 },
+  { "KEY_KP_0",                    (double) GLFW_KEY_KP_0 },
+  { "KEY_KP_1",                    (double) GLFW_KEY_KP_1 },
+  { "KEY_KP_2",                    (double) GLFW_KEY_KP_2 },
+  { "KEY_KP_3",                    (double) GLFW_KEY_KP_3 },
+  { "KEY_KP_4",                    (double) GLFW_KEY_KP_4 },
+  { "KEY_KP_5",                    (double) GLFW_KEY_KP_5 },
+  { "KEY_KP_6",                    (double) GLFW_KEY_KP_6 },
+  { "KEY_KP_7",                    (double) GLFW_KEY_KP_7 },
+  { "KEY_KP_8",                    (double) GLFW_KEY_KP_8 },
+  { "KEY_KP_9",                    (double) GLFW_KEY_KP_9 },
+  { "KEY_KP_DECIMAL",              (double) GLFW_KEY_KP_DECIMAL },
+  { "KEY_KP_DIVIDE",               (double) GLFW_KEY_KP_DIVIDE },
+  { "KEY_KP_MULTIPLY",             (double) GLFW_KEY_KP_MULTIPLY },
+  { "KEY_KP_SUBTRACT",             (double) GLFW_KEY_KP_SUBTRACT },
+  { "KEY_KP_ADD",                  (double) GLFW_KEY_KP_ADD },
+  { "KEY_KP_ENTER",                (double) GLFW_KEY_KP_ENTER },
+  { "KEY_KP_EQUAL",                (double) GLFW_KEY_KP_EQUAL },
+  { "KEY_LEFT_SHIFT",              (double) GLFW_KEY_LEFT_SHIFT },
+  { "KEY_LEFT_CONTROL",            (double) GLFW_KEY_LEFT_CONTROL },
+  { "KEY_LEFT_ALT",                (double) GLFW_KEY_LEFT_ALT },
+  { "KEY_LEFT_SUPER",              (double) GLFW_KEY_LEFT_SUPER },
+  { "KEY_RIGHT_SHIFT",             (double) GLFW_KEY_RIGHT_SHIFT },
+  { "KEY_RIGHT_CONTROL",           (double) GLFW_KEY_RIGHT_CONTROL },
+  { "KEY_RIGHT_ALT",               (double) GLFW_KEY_RIGHT_ALT },
+  { "KEY_RIGHT_SUPER",             (double) GLFW_KEY_RIGHT_SUPER },
+  { "KEY_MENU",                    (double) GLFW_KEY_MENU },
+  { "KEY_LAST",                    (double) GLFW_KEY_LAST },
+  { "MOD_SHIFT",                   (double) GLFW_MOD_SHIFT },
+  { "MOD_CONTROL",                 (double) GLFW_MOD_CONTROL },
+  { "MOD_ALT",                     (double) GLFW_MOD_ALT },
+  { "MOD_SUPER",                   (double) GLFW_MOD_SUPER },
+  { "MOUSE_BUTTON_1",              (double) GLFW_MOUSE_BUTTON_1 },
+  { "MOUSE_BUTTON_2",              (double) GLFW_MOUSE_BUTTON_2 },
+  { "MOUSE_BUTTON_3",              (double) GLFW_MOUSE_BUTTON_3 },
+  { "MOUSE_BUTTON_4",              (double) GLFW_MOUSE_BUTTON_4 },
+  { "MOUSE_BUTTON_5",              (double) GLFW_MOUSE_BUTTON_5 },
+  { "MOUSE_BUTTON_6",              (double) GLFW_MOUSE_BUTTON_6 },
+  { "MOUSE_BUTTON_7",              (double) GLFW_MOUSE_BUTTON_7 },
+  { "MOUSE_BUTTON_8",              (double) GLFW_MOUSE_BUTTON_8 },
+  { "MOUSE_BUTTON_LAST",           (double) GLFW_MOUSE_BUTTON_LAST },
+  { "MOUSE_BUTTON_LEFT",           (double) GLFW_MOUSE_BUTTON_LEFT },
+  { "MOUSE_BUTTON_RIGHT",          (double) GLFW_MOUSE_BUTTON_RIGHT },
+  { "MOUSE_BUTTON_MIDDLE",         (double) GLFW_MOUSE_BUTTON_MIDDLE },
+  { "JOYSTICK_1",                  (double) GLFW_JOYSTICK_1 },
+  { "JOYSTICK_2",                  (double) GLFW_JOYSTICK_2 },
+  { "JOYSTICK_3",                  (double) GLFW_JOYSTICK_3 },
+  { "JOYSTICK_4",                  (double) GLFW_JOYSTICK_4 },
+  { "JOYSTICK_5",                  (double) GLFW_JOYSTICK_5 },
+  { "JOYSTICK_6",                  (double) GLFW_JOYSTICK_6 },
+  { "JOYSTICK_7",                  (double) GLFW_JOYSTICK_7 },
+  { "JOYSTICK_8",                  (double) GLFW_JOYSTICK_8 },
+  { "JOYSTICK_9",                  (double) GLFW_JOYSTICK_9 },
+  { "JOYSTICK_10",                 (double) GLFW_JOYSTICK_10 },
+  { "JOYSTICK_11",                 (double) GLFW_JOYSTICK_11 },
+  { "JOYSTICK_12",                 (double) GLFW_JOYSTICK_12 },
+  { "JOYSTICK_13",                 (double) GLFW_JOYSTICK_13 },
+  { "JOYSTICK_14",                 (double) GLFW_JOYSTICK_14 },
+  { "JOYSTICK_15",                 (double) GLFW_JOYSTICK_15 },
+  { "JOYSTICK_16",                 (double) GLFW_JOYSTICK_16 },
+  { "JOYSTICK_LAST",               (double) GLFW_JOYSTICK_LAST },
+  { "NOT_INITIALIZED",             (double) GLFW_NOT_INITIALIZED },
+  { "NO_CURRENT_CONTEXT",          (double) GLFW_NO_CURRENT_CONTEXT },
+  { "INVALID_ENUM",                (double) GLFW_INVALID_ENUM },
+  { "INVALID_VALUE",               (double) GLFW_INVALID_VALUE },
+  { "OUT_OF_MEMORY",               (double) GLFW_OUT_OF_MEMORY },
+  { "API_UNAVAILABLE",             (double) GLFW_API_UNAVAILABLE },
+  { "VERSION_UNAVAILABLE",         (double) GLFW_VERSION_UNAVAILABLE },
+  { "PLATFORM_ERROR",              (double) GLFW_PLATFORM_ERROR },
+  { "FORMAT_UNAVAILABLE",          (double) GLFW_FORMAT_UNAVAILABLE },
+  { "FOCUSED",                     (double) GLFW_FOCUSED },
+  { "ICONIFIED",                   (double) GLFW_ICONIFIED },
+  { "RESIZABLE",                   (double) GLFW_RESIZABLE },
+  { "VISIBLE",                     (double) GLFW_VISIBLE },
+  { "DECORATED",                   (double) GLFW_DECORATED },
+  { "AUTO_ICONIFY",                (double) GLFW_AUTO_ICONIFY },
+  { "FLOATING",                    (double) GLFW_FLOATING },
+  { "RED_BITS",                    (double) GLFW_RED_BITS },
+  { "GREEN_BITS",                  (double) GLFW_GREEN_BITS },
+  { "BLUE_BITS",                   (double) GLFW_BLUE_BITS },
+  { "ALPHA_BITS",                  (double) GLFW_ALPHA_BITS },
+  { "DEPTH_BITS",                  (double) GLFW_DEPTH_BITS },
+  { "STENCIL_BITS",                (double) GLFW_STENCIL_BITS },
+  { "ACCUM_RED_BITS",              (double) GLFW_ACCUM_RED_BITS },
+  { "ACCUM_GREEN_BITS",            (double) GLFW_ACCUM_GREEN_BITS },
+  { "ACCUM_BLUE_BITS",             (double) GLFW_ACCUM_BLUE_BITS },
+  { "ACCUM_ALPHA_BITS",            (double) GLFW_ACCUM_ALPHA_BITS },
+  { "AUX_BUFFERS",                 (double) GLFW_AUX_BUFFERS },
+  { "STEREO",                      (double) GLFW_STEREO },
+  { "SAMPLES",                     (double) GLFW_SAMPLES },
+  { "SRGB_CAPABLE",                (double) GLFW_SRGB_CAPABLE },
+  { "REFRESH_RATE",                (double) GLFW_REFRESH_RATE },
+  { "DOUBLEBUFFER",                (double) GLFW_DOUBLEBUFFER },
+  { "CLIENT_API",                  (double) GLFW_CLIENT_API },
+  { "CONTEXT_VERSION_MAJOR",       (double) GLFW_CONTEXT_VERSION_MAJOR },
+  { "CONTEXT_VERSION_MINOR",       (double) GLFW_CONTEXT_VERSION_MINOR },
+  { "CONTEXT_REVISION",            (double) GLFW_CONTEXT_REVISION },
+  { "CONTEXT_ROBUSTNESS",          (double) GLFW_CONTEXT_ROBUSTNESS },
+  { "OPENGL_FORWARD_COMPAT",       (double) GLFW_OPENGL_FORWARD_COMPAT },
+  { "OPENGL_DEBUG_CONTEXT",        (double) GLFW_OPENGL_DEBUG_CONTEXT },
+  { "OPENGL_PROFILE",              (double) GLFW_OPENGL_PROFILE },
+  { "CONTEXT_RELEASE_BEHAVIOR",    (double) GLFW_CONTEXT_RELEASE_BEHAVIOR },
+  { "OPENGL_API",                  (double) GLFW_OPENGL_API },
+  { "OPENGL_ES_API",               (double) GLFW_OPENGL_ES_API },
+  { "NO_ROBUSTNESS",               (double) GLFW_NO_ROBUSTNESS },
+  { "NO_RESET_NOTIFICATION",       (double) GLFW_NO_RESET_NOTIFICATION },
+  { "LOSE_CONTEXT_ON_RESET",       (double) GLFW_LOSE_CONTEXT_ON_RESET },
+  { "OPENGL_ANY_PROFILE",          (double) GLFW_OPENGL_ANY_PROFILE },
+  { "OPENGL_CORE_PROFILE",         (double) GLFW_OPENGL_CORE_PROFILE },
+  { "OPENGL_COMPAT_PROFILE",       (double) GLFW_OPENGL_COMPAT_PROFILE },
+  { "CURSOR",                      (double) GLFW_CURSOR },
+  { "STICKY_KEYS",                 (double) GLFW_STICKY_KEYS },
+  { "STICKY_MOUSE_BUTTONS",        (double) GLFW_STICKY_MOUSE_BUTTONS },
+  { "CURSOR_NORMAL",               (double) GLFW_CURSOR_NORMAL },
+  { "CURSOR_HIDDEN",               (double) GLFW_CURSOR_HIDDEN },
+  { "CURSOR_DISABLED",             (double) GLFW_CURSOR_DISABLED },
+  { "ANY_RELEASE_BEHAVIOR",        (double) GLFW_ANY_RELEASE_BEHAVIOR },
+  { "RELEASE_BEHAVIOR_FLUSH",      (double) GLFW_RELEASE_BEHAVIOR_FLUSH },
+  { "RELEASE_BEHAVIOR_NONE",       (double) GLFW_RELEASE_BEHAVIOR_NONE },
+  { "ARROW_CURSOR",                (double) GLFW_ARROW_CURSOR },
+  { "IBEAM_CURSOR",                (double) GLFW_IBEAM_CURSOR },
+  { "CROSSHAIR_CURSOR",            (double) GLFW_CROSSHAIR_CURSOR },
+  { "HAND_CURSOR",                 (double) GLFW_HAND_CURSOR },
+  { "HRESIZE_CURSOR",              (double) GLFW_HRESIZE_CURSOR },
+  { "VRESIZE_CURSOR",              (double) GLFW_VRESIZE_CURSOR },
+  { "CONNECTED",                   (double) GLFW_CONNECTED },
+  { "DISCONNECTED",                (double) GLFW_DISCONNECTED },
+  { "DONT_CARE",                   (double) GLFW_DONT_CARE },
   { NULL, 0.0 }
 };
 
