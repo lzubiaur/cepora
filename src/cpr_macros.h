360ee67accd5eb4fa67c8e5c28fe03a9cbc37ec3
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-18 18:10:07 +0100

    Align macro variable naming

--- src/cpr_macros.h
+++ src/cpr_macros.h
@@ -10,11 +10,11 @@
 #include "duktape.h"
 
 /* Logging macros */
-#define TRC(ctx, ...) duk_log(ctx, DUK_LOG_TRACE, __VA_ARGS__)
-#define DBG(ctx, ...) duk_log(ctx, DUK_LOG_DEBUG, __VA_ARGS__)
-#define INF(ctx, ...) duk_log(ctx, DUK_LOG_INFO,  __VA_ARGS__)
-#define WRN(ctx, ...) duk_log(ctx, DUK_LOG_WARN,  __VA_ARGS__)
-#define ERR(ctx, ...) duk_log(ctx, DUK_LOG_ERROR, __VA_ARGS__)
-#define FTL(ctx, ...) duk_log(ctx, DUK_LOG_FATAL, __VA_ARGS__)
+#define TRC(__ctx__, ...) duk_log(__ctx__, DUK_LOG_TRACE, __VA_ARGS__)
+#define DBG(__ctx__, ...) duk_log(__ctx__, DUK_LOG_DEBUG, __VA_ARGS__)
+#define INF(__ctx__, ...) duk_log(__ctx__, DUK_LOG_INFO,  __VA_ARGS__)
+#define WRN(__ctx__, ...) duk_log(__ctx__, DUK_LOG_WARN,  __VA_ARGS__)
+#define ERR(__ctx__, ...) duk_log(__ctx__, DUK_LOG_ERROR, __VA_ARGS__)
+#define FTL(__ctx__, ...) duk_log(__ctx__, DUK_LOG_FATAL, __VA_ARGS__)
 
 #endif /* CPR_MACROS_H */
