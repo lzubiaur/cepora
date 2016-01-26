44b13d3ab43fa742dff52ac0205625b2dd827fbf
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-26 17:33:02 +0100

    Fix constants undefined

--- src/modules/cpr_dummy.c
+++ src/modules/cpr_dummy.c
@@ -8,17 +8,19 @@ static duk_ret_t foo(duk_context *ctx)
 }
 
 static const duk_function_list_entry module_funcs[] = {
-    { "foo", foo, 1 },
-    { NULL, NULL, 0 }
+  { "foo", foo, 1 },
+  { NULL, NULL, 0 }
 };
 
-const duk_number_list_entry module_consts[] = {
-    { "BAR", (double)(-1) },
-    { NULL, 0.0 }
+/* Linux: if `static` is omitted the constant are `undefined` */
+static const duk_number_list_entry module_consts[] = {
+  { "BAR", (double)(-1) },
+  { NULL, 0.0 }
 };
+
 
 duk_ret_t dukopen_dummy(duk_context *ctx) {
-  DBG(ctx, "dukopen_io");
+  DBG(ctx, "dukopen_dummy");
   duk_push_object(ctx);  /* module result */
   duk_put_function_list(ctx, -1, module_funcs);
   duk_put_number_list(ctx, -1, module_consts);
