556ca4a484a0dfbadfe71ab583208fa0ec58f8d6
lzubiaur <zubiaur.laurent@gmail.com>
2016-01-29 21:29:17 +0100

    Fix gl3w build on OSX

--- bin/build-osx.sh
+++ bin/build-osx.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# generate GL3W if needed
+if ! [ -f "lib/gl3w/src/gl3w.c" ]; then
+  pushd lib/gl3w
+  ./gl3w_gen.py
+  popd
+fi
+
 if ! [ -d "build/osx" ]
 then
    mkdir -p "build/osx"
 fi
 cd build/osx
 
-# Generate build system using the XCode generator.
-# Should also work using the "Unix Makefiles" generator.
-#cmake -G "Xcode" \
-cmake -G "Unix Makefiles" \
+# Generate build system using the XCode generator. "Unix Makefiles" might not
+# because of third party library like GL3W who requires objective-c.
+cmake -G "Xcode" \
     -DBUILD_OSX=TRUE \
     -DCMAKE_BUILD_TYPE=Release \
     ../..
