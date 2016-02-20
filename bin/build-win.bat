c9e71924e13e7eaa5d057869c008b648f66c6c9a
lzubiaur <zubiaur.laurent@gmail.com>
2016-02-20 17:55:09 +0100

    Add debug build option

--- bin/build-win.bat
+++ bin/build-win.bat
@@ -7,20 +7,27 @@ if not exist lib\gl3w\src\gl3w.c (
 )
 
 if not exist build\win (
-    mkdir build\win
-    )
+  mkdir build\win
+)
 
 pushd build\win
 
 set PATH=.;C:\Users\lzubiaur\cmake-3.3.2\bin;%PATH% 
 
+set BUILD_TYPE=Release
+
+if /I "%1" == "debug" (
+  set BUILD_TYPE=Debug
+  echo "Build DEBUG"
+)
+
 cmake.exe ^
     -G "Visual Studio 14 2015" ^
     -DBUILD_WIN=TRUE ^
     -DCMAKE_SYSTEM_VERSION=10.0 ^
-    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     ..\..
 
-cmake.exe --build . --target install --config Release
+cmake.exe --build . --target install --config %BUILD_TYPE%
 
 popd
