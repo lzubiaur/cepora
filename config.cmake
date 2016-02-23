6e5b359a6907179177be6525473a999865758a00
lzubiaur <zubiaur.laurent@gmail.com>
2016-02-23 14:49:57 +0100

    Add option CPR_WINDOWS_INSTALL_CRT

--- config.cmake
+++ config.cmake
@@ -5,4 +5,9 @@ set(CPR_BUILD_DEBUG_INTERNAL OFF)
 # Run Make in verbose mode
 set(CMAKE_VERBOSE_MAKEFILE OFF)
 
+# Windows
+# Install redistributable runtime (CRT)
+set(CPR_WINDOWS_INSTALL_CRT OFF)
+
+
 # TODO modules flags
